3669623acc0f383076ae3b27d710ca9674c66d7c
jonP <kusala9@googlemail.com>
2019-12-04 10:42:21 +0000

    day4....

---
+++ AoC/src/d4.java
@@ -0,0 +1,13 @@
+/*
+   DOCO:  
+*/
+
+public class d4
+{
+    public static void main(String []a)
+    {
+        System.out.println("Hello world");
+        System.out.println("DAy4....");
+    }
+
+}
