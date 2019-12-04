f956c6c636811759bc58102ed59042c22fca0b7f
kusala9 <jp@jp.com>
2019-12-04 07:59:16 +0000

    day 2.1

--- AoC/src/d2.java
+++ AoC/src/d2.java
@@ -4,32 +4,21 @@ public class d2
 
 
      */
-    public static void f(int []d)
-    {
-        System.out.print("(");
-        for(int i:d) System.out.print(i+",");
-        System.out.println(")");
-    }
-
-    public static void main(String []a)
+    public static int runC(int noun,int verb)
     {
         int []td = {1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,5,19,23,2,9,23,27,1,27,5,31,2,31,13,35,1,35,9,39,1,39,10,43,2,43,9,47,1,47,5,51,2,13,51,55,1,9,55,59,1,5,59,63,2,6,63,67,1,5,67,71,1,6,71,75,2,9,75,79,1,79,13,83,1,83,13,87,1,87,5,91,1,6,91,95,2,95,13,99,2,13,99,103,1,5,103,107,1,107,10,111,1,111,13,115,1,10,115,119,1,9,119,123,2,6,123,127,1,5,127,131,2,6,131,135,1,135,2,139,1,139,9,0,99,2,14,0,0};
-        //int []td = {1,1,1,4,99,5,6,0,99};
-        //int []td = {12,14,1969,100756};
-        System.out.println("hello world...");
-        System.out.println("advent challenge 2");
-
+        td[1] = noun;
+        td[2] = verb;
         int pos=0;
-
         while (true)
         {
             int d = td[pos];
             if (d==99) break;
-            System.out.println(td[pos] + "," + td[pos+1] + "," + td[pos+2] + "," + td[pos+3]);
+            //System.out.println(td[pos] + "," + td[pos+1] + "," + td[pos+2] + "," + td[pos+3]);
             if (d==1)
             {
-               int j=td[td[pos+1]] + td[td[pos+2]];
-               td[td[pos+3]] = j;
+                int j=td[td[pos+1]] + td[td[pos+2]];
+                td[td[pos+3]] = j;
             }
             if (d==2)
             {
@@ -37,9 +26,34 @@ public class d2
                 td[td[pos+3]] = j;
             }
             pos+=4;
-            f(td);
         }
+        //f(td);
+        return td[0];
+    }
+    public static void f(int []d)
+    {
+        System.out.print("{");
+        for(int i:d) System.out.print(i+",");
+        System.out.println("}");
+    }
 
-        //System.out.println("=" + m);
+    public static void main(String []a)
+    {
+
+        System.out.println("hello world...");
+        System.out.println("advent challenge 2");
+
+        int noun = 10;
+        int verb = 4;
+        int max=100;
+        for (int i=0;i<max;i++)
+        {
+            for (int j=0;j<max;j++)
+            {
+                int ret = runC(i,j);
+                System.out.println(i + ":" + j + "=" + ret);
+                if (ret == 19690720) System.exit(0);
+            }
+        }
     }
 }
