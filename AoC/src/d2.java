0d61c4e3b3818abe13bde5a742adcceb810006a4
kusala9 <jp@jp.com>
2019-12-04 07:42:07 +0000

    day 2.1

--- AoC/src/d2.java
+++ AoC/src/d2.java
@@ -2,33 +2,44 @@ public class d2
 {
     /*
 
-    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
-    At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
-    The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
 
      */
-    public static int f(int j,int rt)
+    public static void f(int []d)
     {
-        int n = (j/3)-2;
-        System.out.println("   " + n + ":" + rt);
-        if (n<0) return rt;
-        else return f(n,(n+rt));
+        System.out.print("(");
+        for(int i:d) System.out.print(i+",");
+        System.out.println(")");
     }
 
     public static void main(String []a)
     {
-        int []data = {53035, 84950, 100012, 75128, 96658, 80583, 72951, 131866, 99675, 115923, 65140, 59430, 81288, 53129, 96172, 58912, 138055, 62920, 122353, 59176, 149310, 105061, 58808, 103111, 128538, 61366, 53480, 94427, 121742, 143966, 63092, 92543, 67136, 81575, 131971, 71040, 57035, 114448, 101314, 123688, 137916, 68612, 122453, 98141, 61137, 97628, 126791, 111826, 50630, 67829, 126285, 97331, 88587, 64552, 111221, 89639, 72177, 132711, 51062, 98061, 57919, 57166, 134565, 58677, 62505, 85253, 147337, 84791, 114516, 95597, 139271, 83561, 68285, 100684, 86803, 85887, 74554, 113749, 81899, 107220, 110959, 118220, 52743, 71651, 74775, 106517, 132133, 56919, 129699, 137357, 75781, 59409, 134589, 131438, 101641, 105503, 104371, 145308, 75777, 107333} ;
+        int []td = {1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,5,19,23,2,9,23,27,1,27,5,31,2,31,13,35,1,35,9,39,1,39,10,43,2,43,9,47,1,47,5,51,2,13,51,55,1,9,55,59,1,5,59,63,2,6,63,67,1,5,67,71,1,6,71,75,2,9,75,79,1,79,13,83,1,83,13,87,1,87,5,91,1,6,91,95,2,95,13,99,2,13,99,103,1,5,103,107,1,107,10,111,1,111,13,115,1,10,115,119,1,9,119,123,2,6,123,127,1,5,127,131,2,6,131,135,1,135,2,139,1,139,9,0,99,2,14,0,0};
+        //int []td = {1,1,1,4,99,5,6,0,99};
         //int []td = {12,14,1969,100756};
         System.out.println("hello world...");
         System.out.println("advent challenge 2");
 
-        int m=0;
-        for (int i:data)
+        int pos=0;
+
+        while (true)
         {
-            int j= f(i,0);
-            System.out.println("=" + j);
-            m+=j;
+            int d = td[pos];
+            if (d==99) break;
+            System.out.println(td[pos] + "," + td[pos+1] + "," + td[pos+2] + "," + td[pos+3]);
+            if (d==1)
+            {
+               int j=td[td[pos+1]] + td[td[pos+2]];
+               td[td[pos+3]] = j;
+            }
+            if (d==2)
+            {
+                int j=td[td[pos+1]] * td[td[pos+2]];
+                td[td[pos+3]] = j;
+            }
+            pos+=4;
+            f(td);
         }
-        System.out.println("=" + m);
+
+        //System.out.println("=" + m);
     }
 }
