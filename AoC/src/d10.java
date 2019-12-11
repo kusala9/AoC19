76417b55c1aaa5de170e98350e38a6ad4e3a2705
kusala9 <jp@jp.com>
2019-12-11 19:09:21 +0000

    d7.1 - (with a bit of help)

--- AoC/src/d10.java
+++ AoC/src/d10.java
@@ -3,14 +3,21 @@ import java.util.HashMap;
 
 public class d10
 {
-    public static int gcf(int num1,int num2)
+    public static int gcf(int no1,int no2)
     {
-        int g=0;
-        for(int i = 1; i <= num1 && i <= num2; i++)
+        int n1 = Math.abs(no1);
+        int n2 = Math.abs(no2);
+        if (n1==0) return n2;
+        if (n2==0) return n1;
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++)
         {
-            if(num1%i==0 && num2%i==0) g=i;
+            if (n1 % i == 0 && n2 % i == 0)
+            {
+                gcd = i;
+            }
         }
-        return g;
+        return gcd;
     }
     public static boolean isM(pear<Integer,Integer> e1,pear<Integer,Integer> e2,HashMap<pear<Integer,Integer>,Boolean> m3)
     {
@@ -18,25 +25,41 @@ public class d10
 
         int dx = e2.first - e1.first;
         int dy = e2.second - e1.second;
+        if (e1.equals(e2)) return true;
+//        int g = 0;
+//        if (dx==0||dy==0) g=1;
         int g = gcf(dx,dy);
-        if (e1 == e2) return true;
-        if (g==1) return true;
+
+        if ( (dx!=0 && dy!=0 ) && (g ==1))
+        {
+            return true;
+        }
+
+        int incx = dx/g;
+        int incy = dy/g;
 
         // go from e1 to e2.
-        while (e1 != e2)
+
+        while (true)
         {
-            e1 = new pear<Integer,Integer>(e1.first+dx,e1.second+dy);
-            if (m3.containsKey(e1)) return false;
+            pear<Integer,Integer> e3 = new pear<Integer,Integer>(e1.first+incx,e1.second+incy);
+            if (e3.equals(e2)) break;
+            if (m3.containsKey(e3))
+            {
+                return false;
+            }
+            e1 = e3;
         }
         return true;
     }
     public static void main(String []a)
     {
         ArrayList<String> m = d10d.getd2();
-        pear<Integer,Integer> p = new pear<Integer,Integer>(1,0);
+        pear<Integer,Integer> p = new pear<Integer,Integer>(3,4);
         HashMap<String,Integer> m2 = new HashMap<>();
         HashMap<pear<Integer,Integer>,Boolean> m3 = new HashMap<>();
 
+        int tot=0;
         for (int i=0;i<m.size();i++)
         {
             String s = m.get(i);
@@ -46,13 +69,17 @@ public class d10
                 if (s.charAt(j)=='#')
                 {
                     //System.out.println("A at(" + i + "," + j + ")");
-                    p = new pear<>(i,j);
+                    p = new pear<>(j,i);
                     m3.put(p,true);
+                    tot++;
                 }
             }
         }
 
         int max=0;
+        pear<Integer,Integer> trace = new pear<Integer,Integer>(6,3);
+        HashMap<pear<Integer,Integer>,Boolean> tracemap = new HashMap<>();
+
         for (pear<Integer,Integer> e1: m3.keySet())
         {
             int cnt=0;
@@ -60,15 +87,50 @@ public class d10
             {
                 if (isM(e1,e2,m3))
                 {
-                    //System.out.println("Match => " + e1 + "-" + e2);
+                    //System.out.println(e1 + "Match-" + e2);
+                    if (e1.equals(trace)) tracemap.put(e2,true);
                     cnt++;
                 }
+                else
+                {
+                    //System.out.println(e1 + " Not match-" + e2);
+                    if (e1.equals(trace)) tracemap.put(e2,false);
+                }
             }
-            if (cnt>max)
+            if (cnt>=max)
             {
                 System.out.println("New Max = " + e1 + "=" + cnt);
                 max=cnt;
             }
+        }
+
+        for (int i=0;i<m.size();i++)
+        {
+            String s = m.get(i);
+            for (int j=0;j<s.length();j++)
+            {
+                if (s.charAt(j)=='#')
+                {
+                    p = new pear<>(j,i);
+                    if (tracemap.containsKey(p))
+                    {
+                        if (p.equals(trace)) System.out.print("O");
+                        else if (tracemap.get(p)==true) System.out.print("X");
+                        else System.out.print("x");
+                    }
+                    else
+                    {
+                        System.out.print("_");
+                    }
+                    m3.put(p,true);
+                    tot++;
+                }
+                else
+                {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
         }
 
 
